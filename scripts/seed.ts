a57ee713fa2fcd20ef04f90c68fa8e3c4be21bcb
Thomas Seillan <thomas.seillan@gmail.com>
2022-08-10 14:00:10 +0200

    Change seed script to empty DB - temporary fix to cleanup online db.

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -3,9 +3,33 @@ import skills from './seed/skills'
 import skillsets from './seed/skillsets'
 import topics from './seed/topics'
 import ideas from './seed/ideas'
+import { db } from '$api/src/lib/db'
 
 export default async () => {
   try {
+    console.warn(
+      `
+This command is pruning your entire DB before seeding anything.
+Sorry about that - it's necessary to clean up online environment. We'll sort it out soon!
+
+
+`
+    )
+
+    await db.topic.deleteMany()
+
+    await db.skillSet.deleteMany()
+
+    await db.skill.deleteMany()
+
+    await db.member.deleteMany()
+
+    await db.ideaVote.deleteMany()
+
+    await db.idea.deleteMany()
+
+    await db.user.deleteMany()
+
     await topics()
 
     await skillsets()
